932b986055f1d68961c8a6d69fd09d11f83ef51b
Felipe A. Soares <felipesoares31481@gmail.com>
2020-09-09 15:54:51 -0300

    Base project

---
+++ src/main/java/br/com/fas/usersregistry/UsersRegistryApplication.java
@@ -0,0 +1,13 @@
+package br.com.fas.usersregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersRegistryApplication.class, args);
+	}
+
+}
